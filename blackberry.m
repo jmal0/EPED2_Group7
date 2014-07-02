de7542f98039a227c66a19a41951e7641c9d5d06
John Maloney <jmal0320@gmail.com>
2014-07-02 18:22:55 -0400

    I now hate me

--- blackberry.m
+++ blackberry.m
@@ -1,44 +1,39 @@
-clear all;close all;clc;
-format long;
+clear all;close all
+clc;
 
+% same as Chevron analysis but for Blackberry stock
+[ndata, text, allData] = xlsread('Blackberry.xlsx');
 
-% extract Chevron datas
-[ndata, text, allData] = xlsread('Chevron.xlsx');
-
-% convert dates from number dates to date strings
 dates = flipud(text(2:end,1));
-
 closing = flipud(ndata(:,4));
 
 maximum = max(closing);
 minimum = min(closing);
-range_ = range(closing);
+range = range(closing);
 avg = mean(closing);
-binsize = ceil(range_/10);
+binsize = ceil(range/10);
 
-figure;
+figure(5);
 
-% plot stock prices on same figure
 subplot(2,1,1);
 plot(datenum(dates), closing);
 grid on;
 datetick('x');
 xlabel('Date');
 ylabel('Closing Stock Price ($)');
-title('Chevron Stock Price in 2013');
+title('Blackberry Stock Price in 2013');
 
 hold on;
 
-% plot histogram on same figure
 subplot(2,1,2);
 hist(closing, floor(minimum):binsize:ceil(maximum));
 grid on;
-title('Chevron Stock Price Distribution in 2013');
-legend(sprintf('Bin size: %.0d', binsize));
+title('Blackberry Stock Price Distribution in 2013');
+legend(sprintf('Bin size: $%.0d', binsize));
 ylabel('Frequency');
 xlabel('Stock Price Range ($)');
 
 fprintf('Min: $%.2f\n', minimum);
 fprintf('Max: $%.2f\n', maximum);
-fprintf('Range: $%.2f\n', range_);
+fprintf('Range: $%.2f\n', range);
 fprintf('Mean: $%.2f\n', avg);
