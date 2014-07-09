51167c419d28080b493dbe977c1f99742151248b
Leonard Chan <lc599@drexel.edu>
2014-07-09 18:47:41 -0400

    Update wk3_vector.m

--- Biweekly2/StudentAccelerometer/StudentAccelerometer/wk3_vector.m
+++ Biweekly2/StudentAccelerometer/StudentAccelerometer/wk3_vector.m
@@ -15,10 +15,15 @@
 
 
 %% 1. Specifies the COM port that the Arduino board is connected to
-comPort = 'COM27';%This can be found out using the device manager (Windows)
-                  %On a mac type ls /dev/tty* in Terminal and 
-                  %  identify the device that is listed as usbmodem
-                  %  Example for a MAC comPort = '/dev/tty.usbmodemfa131';
+
+
+comPort = 'COM6'; % comPort was initially COM27, but the acceleromater was attached to COM6, so we changed it to make the code work
+
+
+%This can be found out using the device manager (Windows)
+%On a mac type ls /dev/tty* in Terminal and 
+%  identify the device that is listed as usbmodem
+%  Example for a MAC comPort = '/dev/tty.usbmodemfa131';
 %comPort = '/dev/tty.usbmodemfd121';
 
 
@@ -67,7 +72,7 @@ end
 %% 5. Runs a loop that continually reads the accelerometer values 
 %     readAcc() - (not to be altered)
 %     The accelerometer data is placed in the variables [gx gy gz].
- 
+
 while (get(button, 'UserData'))
    
     %read accelerometer output
@@ -75,15 +80,21 @@ while (get(button, 'UserData'))
     
     cla;            %clear everything from the current axis
     
-    %plot X and Y acceleration vectors and resultant acceleration vector
-    line([0 gx], [0 0], 'Color', 'r', 'LineWidth', 2, 'Marker', 'o');
+    %plot all 3 axis components of accelerometer (X, Y, Z) and resultant vector
+    line([0 gx], [0 0], 'Color', 'r', 'LineWidth', 2, 'Marker', 'o'); % x component of accelerometer in red
+    line([0 0], [0 gy], 'Color', 'g', 'LineWidth', 2, 'Marker', 'o'); % y component of accelerometer in green
+    line([0 0], [0 0], [0 gz], 'Color', 'b', 'LineWidth', 2, 'Marker', 'o'); % z component of accelerometer in blue
+    line([0 gx], [0 gy], [0 gz], 'Color', 'k', 'LineWidth', 2, 'Marker', 'o'); % resultant acceleration in black
     
-    
-    
-
-    %limit plot to +/- 1.25 g in all directions and make axis square
+    %limit plot to +/- 2.5 g in all directions and make axis square
     limits = 2.5;
-    axis([-limits limits -limits limits]);
+    
+    % added 2 more sets of limits to show 3d plot
+    axis([-limits limits -limits limits -limits limits]);
+    grid on; % don't forget to show gridlines
+    xlabel('Acceleration (g) in X axis'); % label for x axis
+    ylabel('Acceleration (g) in Y axis'); % label for y axis
+    zlabel('Acceleration (g) in Z axis'); % label for z axis
     axis square;
     
     %calculate the angle of the resultant acceleration vector and print
